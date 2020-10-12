37fcc994be4f648f25b50632458d21c71f86adc6
DanielO3050 <63265300+DanielO3050@users.noreply.github.com>
2020-10-12 12:20:17 -0300

    Initial commit

--- app/src/main/java/com/example/biblioteca/github_act.java
+++ app/src/main/java/com/example/biblioteca/github_act.java
@@ -6,28 +6,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class github_act extends AppCompatActivity {
 
     private Spinner spinner;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_github_act);
 
         spinner= (Spinner)findViewById(R.id.spinner);
+        textView =(TextView)findViewById(R.id.t2);
 
         String[] lista1 = {"Farenheith", "Revival", "El Alquimista"};
 
         ArrayAdapter<String> adap = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lista1);
         spinner.setAdapter(adap);
-    }
 
-    public void libros(View v)
-    {
-        int Farenheith= 5000;
-        int Revival = 12000;
-        int El_Alquimista = 45000;
+        String libro = spinner.getSelectedItem().toString();
+        int CFa= 5000;
+        int CRe = 12000;
+        int CAL = 45000;
+
+        if (libro.equals("Farenheith"))
+        {
+            textView.setText(new StringBuilder().append("El costo de ").append("Farenheith ").append("Es de ").append(CFa).toString());
+        }
+        else if (libro.equals("Revival"))
+        {
+            textView.setText(new StringBuilder().append("El costo de ").append("Revival ").append("Es de ").append(CRe).toString());
+        }
+        else if (libro.equals("El Alquimista"))
+        {
+            textView.setText(new StringBuilder().append("El costo de ").append("El Alquimista ").append("Es de ").append(CAL).toString());
+        }
     }
 
 }
